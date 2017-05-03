ee3a4d66331fdb0d69466db5ca5428e688644c8c
Klaudyna <klaudynaz@gmail.com>
2017-05-03 11:14:14 +0200

    Usuwam projekt bez możliwości logowania

--- MachinesApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MachinesApp.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- MachinesApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MachinesApp.WebApiApplication" Language="C#" %>
